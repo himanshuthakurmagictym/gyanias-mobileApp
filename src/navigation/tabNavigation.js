d9911b30b4bdcb816699ede95818d8be7af2b169
Magictym <magictym@outlook.com>
2022-06-16 11:31:21 +0530

    header setting

--- src/navigation/tabNavigation.js
+++ src/navigation/tabNavigation.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {TouchableOpacity, Alert} from 'react-native'
+import {TouchableOpacity, Alert, Text} from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../screens/Home';
@@ -9,6 +9,7 @@ import LogoTitle from '../screens/LogoTitle';
 import  Icon  from 'react-native-vector-icons/FontAwesome'
 import  Icon2  from 'react-native-vector-icons/Fontisto'
 import  Icon3  from 'react-native-vector-icons/AntDesign'
+import  Icon4 from 'react-native-vector-icons/Feather'
 import Course from '../screens/Course';
 import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
 import HomeStack from './HomeStack';
@@ -17,37 +18,42 @@ const Tab = createBottomTabNavigator();
 const TabNavigation = ({ navigation, route, options })=>{
 
    
-    function getHeaderTitle(route) {
-        console.log(route)
-        const routeName = getFocusedRouteNameFromRoute(route) ?? 'Home';
-        switch (routeName) {
-          case 'Course':
-            return 'Course';
-        }
-      } 
+//     function getHeaderTitle(route) {
+//         console.log(route)
+//         const routeName = getFocusedRouteNameFromRoute(route) ?? 'Home';
+//         switch (routeName) {
+//           case 'Course':
+//             return 'Course';
+//         }
+//       } 
 
-    React.useLayoutEffect(() => {
-    navigation.setOptions({  title: getHeaderTitle(route)  });
-  }, [navigation, route]);
+//     React.useLayoutEffect(() => {
+//     navigation.setOptions({  title: getHeaderTitle(route)  });
+//   }, [navigation, route]);
   
     return(
        
         <Tab.Navigator  screenOptions={{
-            tabBarStyle:{backgroundColor:"#007bff"}, tabBarActiveTintColor:'white', tabBarInactiveTintColor:'white', headerTitleAlign:'center', tabBarShowLabel:false
+            tabBarStyle:{backgroundColor:"#007bff"}, tabBarActiveTintColor:'white', tabBarInactiveTintColor:'white', headerTitleAlign:'center', tabBarShowLabel:false,
+           
         }}>     
         <Tab.Screen name="home" component={Home} options={({route}) =>({tabBarIcon: ({color, size})=>(
             <Icon name="home" size={24} color='#fff' />
-            ), headerTitle: props => <LogoTitle {...props} />, headerShown: false,})} 
+            ), headerTitle: props => <LogoTitle {...props} />, headerShown: true, headerLeft:()=>(  <TouchableOpacity onPress={()=>{navigation.toggleDrawer()}}  >
+            <Icon4 name="menu" size={24}  style={{paddingLeft:15}}/>
+            </TouchableOpacity>),  })} 
         />   
         <Tab.Screen name="Course" component={Course} options={{tabBarIcon: ({color, size})=>(
             <Icon2 name="person" size={24} color='#fff' />
-        ), headerTitle: props => <LogoTitle {...props} />, headerShown: false}}/>
+        ), headerTitle: props => <LogoTitle {...props} />, headerShown: true,  headerLeft:()=>(<TouchableOpacity onPress={()=>{navigation.toggleDrawer()}}  >
+        <Icon4 name="menu" size={24}  style={{paddingLeft:15}}/>
+        </TouchableOpacity>)}}/>
         
         <Tab.Screen name="toggleDrawer"  component={Course} options={{tabBarIcon: ({color, size})=>(
             <Icon3 name="menufold" size={24} color='#fff' />
          ), headerTitle: props => <LogoTitle {...props} />, tabBarButton: props => (
             <TouchableOpacity {...props} onPress={() => navigation.toggleDrawer()} />
-          ),}}/>
+          )}}/>
     </Tab.Navigator>
     )
 }
--- src/screens/LogoTitle.js
+++ src/screens/LogoTitle.js
@@ -1,10 +1,12 @@
 import React from  'react';
-import {Text, View, Image, StyleSheet} from 'react-native'
+import {Text, View, Image, StyleSheet, TouchableOpacity} from 'react-native'
 import  Icon  from 'react-native-vector-icons/FontAwesome'
+import  Icon2  from 'react-native-vector-icons/Feather'
  const LogoTitle = (props)=>{
   
     return(
         <View style={{flex:1, flexDirection:'row', justifyContent:'space-between', alignItems:'center', width:'100%'}}>
+        
             <Image source={require("../../assets/img/logo.png")} style={{width:35, height:35}}/>
             <Text style={styles.header}>{props.children}</Text>
             <Icon name="bell" size={25} color='#007bff' />
